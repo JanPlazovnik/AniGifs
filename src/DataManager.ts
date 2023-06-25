c2d7d1afb97f442dcb82d1cd44aedece0edf5c6a
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-06-25 14:12:11 +0200

    revert id changes and hardcode latest post

--- src/DataManager.ts
+++ src/DataManager.ts
@@ -1,10 +1,12 @@
 import { ICatResponse } from "./types/cat";
 import { IOtaGifsPost } from "./types/otagifs";
 
-type CachedPost = IOtaGifsPost | ICatResponse;
+export type CachedPost = IOtaGifsPost | ICatResponse;
 
 export default class DataManager {
-  private postCache: Map<string, CachedPost> = new Map();
+  private postCache: Map<string, CachedPost> = new Map([
+    ["otagifs_Cn3pei", {} as CachedPost], // Hardcoded to prevent duplicate right off the bat.
+  ]);
 
   public isPostCached(id: string): boolean {
     return this.postCache.has(id);
@@ -13,8 +15,6 @@ export default class DataManager {
   public cachePost(post: CachedPost): void {
     if ("id" in post) {
       this.postCache.set(post.id, post);
-    } else if ("_id" in post) {
-      this.postCache.set(post._id, post);
     } else {
       throw new Error("Invalid post object.");
     }
