e20338274aa1292d936f972ff0f3f176a2183221
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-06-25 14:12:23 +0200

    add dev mode option

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import * as cron from "cron";
 import AnilistService from "./services/AnilistService";
 
+export const DEV_MODE = false;
+
 (() => {
   console.log("Starting a CRON job");
   const anilist = new AnilistService();
-  const job = new cron.CronJob(
-    "0 20 * * *",
-    async () => await anilist.addActivity(),
-    null,
-    true,
-    "Europe/Ljubljana"
-  );
-  job.start();
-  console.log(`Job started, next run is at ${job.nextDate().toLocaleString()}`);
+
+  if (!DEV_MODE) {
+    console.log("Running in production mode.");
+    const job = new cron.CronJob(
+      "0 20 * * *",
+      async () => await anilist.addActivity(),
+      null,
+      true,
+      "Europe/Ljubljana"
+    );
+    job.start();
+    console.log(
+      `Job started, next run is at ${job.nextDate().toLocaleString()}`
+    );
+  } else {
+    console.log("Running in development mode.");
+    anilist.addActivity();
+  }
 })();
