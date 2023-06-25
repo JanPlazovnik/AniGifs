a6fe2dbd97f9945bf4628511f8c68130c503aacd
Jan Plazovnik <jan.plazovnik@gmail.com>
2023-06-25 13:33:21 +0200

    allow cat posts in cache

--- src/DataManager.ts
+++ src/DataManager.ts
@@ -1,13 +1,22 @@
-import { IPost } from "./types/otagifs";
+import { ICatResponse } from "./types/cat";
+import { IOtaGifsPost } from "./types/otagifs";
+
+type CachedPost = IOtaGifsPost | ICatResponse;
 
 export default class DataManager {
-  private postCache: Map<string, IPost> = new Map();
+  private postCache: Map<string, CachedPost> = new Map();
 
   public isPostCached(id: string): boolean {
     return this.postCache.has(id);
   }
 
-  public cachePost(post: IPost): void {
-    this.postCache.set(post.id, post);
+  public cachePost(post: CachedPost): void {
+    if ("id" in post) {
+      this.postCache.set(post.id, post);
+    } else if ("_id" in post) {
+      this.postCache.set(post._id, post);
+    } else {
+      throw new Error("Invalid post object.");
+    }
   }
 }
